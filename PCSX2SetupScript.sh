70bc864dea1f6730d5572c36c29795f0899297e3
Cepheus <83772417+Cepheus001@users.noreply.github.com>
2023-06-27 13:29:37 -0400

    Add files via upload

--- PCSX2SetupScript.sh
+++ PCSX2SetupScript.sh
@@ -2,6 +2,68 @@
 
 #config menu here
 
+# Main function here.
+
+function main () {
+	echo "Install one of the following versions of PCSX2:
+	1 - Install apt version.
+	2 - Install Flatpak version.
+	3 - Install Flatpak along with PCSX2.
+	4 - Configurations Menu
+	5 - Run PCSX2
+	6 - Exit"
+	read -p "Which version of PCSX2 would you like to install?" choice01
+	
+	case $choice01 in
+		1 ) echo 'Ok, installing apt version of PCSX2.';
+			install_pcsx2viaapt
+			break;;
+		2 ) echo 'Ok, installing the Flatpak version of PCSX2.';
+			install_pcsx2viaflatapak
+			break;;
+		3 ) echo 'Ok, Installing Flatpak with PCSX2.';
+			install_flatpak;
+			break;;
+		4 ) echo 'Ok, choosing the config menu. ';
+			configmenu;;
+		5 ) echo 'Ok, running PCSX2.';
+			read -p 'Flatpak or apt?
+			1 - flatpak
+			2 - apt
+			3 - return home' yn6
+			case $yn6 in
+				1 ) clear;
+					launchpcsx2flatpak
+					break;;
+				2 ) clear;
+					launchpcsx2apt
+					break;;
+				3 ) clear;
+					main
+					break;;
+			esac
+			break;;
+		6 ) echo 'Exit';
+			clear;
+			read -p 'Are you sure you want to exit? [Y/N] ' yn
+				case $yn in
+					[Yy] ) echo 'Exiting...';
+						clear;
+						exit;;
+					[Nn] ) echo 'Returning...';
+						clear;
+						main
+						break;;
+				esac
+			break;;
+		* ) echo 'invalid response';
+		    clear;
+		    main
+		    break;;
+	esac
+}
+
+
 function configmenu () {
 	echo "Install one of the following versions of PCSX2:
 	1 - Add BIOS files
@@ -22,6 +84,26 @@ function configmenu () {
 		3 ) echo 'Configuring Network....';
 			networking
 			break;;
+		4 ) echo 'Configuring Graphics....';
+			configgraphics
+			break;;
+		5 ) echo 'Ok, running PCSX2.';
+			read -p 'Flatpak or apt?
+			1 - flatpak
+			2 - apt
+			3 - return home' yn6
+			case $yn6 in
+				1 ) clear;
+					launchpcsx2flatpak
+					break;;
+				2 ) clear;
+					launchpcsx2apt
+					break;;
+				3 ) clear;
+					main
+					break;;
+			esac
+			break;;
 		6 ) echo 'Exit';
 			clear;
 			read -p 'Are you sure you want to exit? [Y/N] ' yn
@@ -33,23 +115,31 @@ function configmenu () {
 						clear;
 						main
 						break;;
+				esac
+				break;;
 		* ) echo 'invalid response';
 		    clear;
 		    configmenu
 		    break;;
-		esac
+	esac
 }
 
-# configuration methods here 
-
+# configuration methods here for flatpak
+<<COMMENT
 function configgraphics () {
-	
+
 }
+
+function networking () {
+
+}
+
+COMMENT
 
 function addroms () {
 	read -p "Enter the Directory in which your roms are stored to be copied over to the install directory: " enterRoms
 	sudo mkdir $HOME/.var/app/net.pcsx2.PCSX2/roms ; sudo mv -r $enterRoms $HOME/.var/app/net.pcsx2.PCSX2/roms ; sudo sed -i 's/RecursivePaths =/$enterRoms/g' $HOME/.var/app/net.pcsx2.PCSX2/config/PCSX2/inis/PCSX2.ini
-	addbios
+	configmenu
 
 }
 
@@ -58,7 +148,7 @@ function addbios () {
 
 	case $yn3 in
 		[yY] ) echo 'Please enter the path for your BIOS';
-			read -p biospath;
+			read -p 'Enter Path to File(s): ' biospath;
 			sudo cp -r '$biospath' '$HOME/.var/app/net.pcsx2.PCSX2/config/PCSX2/bios';
 			exit;;
 		[nN] ) echo 'Bye!';
@@ -67,18 +157,15 @@ function addbios () {
 			exit;;
 	esac
 
-function networking () {
-
 }
 
-}
-
-# Launch methods here.
+# Launch methods here for flatpak and apt.
 
 function launchpcsx2apt () {
-	read -p "would you like to Run PCSX2?:
+	echo "would you like to Run PCSX2?:
 	1 - Run PCSX2
-	2 - return back to main menu " c12
+	2 - return back to main menu " 
+	read -p 'Enter: ' c12
 
 	case $c12 in
 		1 ) "Running....";
@@ -124,67 +211,6 @@ function install_pcsx2viaflatpak () {
     echo 'Installing PCSX2 now via Flatpak....';
     sudo flatpak install net.pcsx2.PCSX2 -y;
 
-}
-
-# Main function here.
-
-function main () {
-	echo "Install one of the following versions of PCSX2:
-	1 - Install apt version.
-	2 - Install Flatpak version.
-	3 - Install Flatpak along with PCSX2.
-	4 - Configurations Menu
-	5 - Run PCSX2
-	6 - Exit"
-	read -p "Which version of PCSX2 would you like to install?" choice01
-	
-	case $choice01 in
-		1 ) echo 'Ok, installing apt version of PCSX2.';
-			install_pcsx2viaapt
-			break;;
-		2 ) echo 'Ok, installing the Flatpak version of PCSX2.';
-			install_pcsx2viaflatapak
-			break;;
-		3 ) echo 'Ok, Installing Flatpak with PCSX2.';
-			install_flatpak;
-			break;;
-		4 ) echo 'Ok, choosing the config menu. ';
-			configmenu;;
-		5 ) echo 'Ok, running PCSX2';
-			read -p 'Flatpak or apt?
-			1 - flatpak
-			2 - apt
-			3 - return home' yn6
-			case $yn6 in
-				1 ) clear;
-					launchpcsx2flatpak
-					break;;
-				2 ) clear;
-					launchpcsx2apt
-					break;;
-				3 ) clear;
-					main
-					break;;
-			esac
-			break;;
-		6 ) echo 'Exit';
-			clear;
-			read -p 'Are you sure you want to exit? [Y/N] ' yn
-				case $yn in
-					[Yy] ) echo 'Exiting...';
-						clear;
-						exit;;
-					[Nn] ) echo 'Returning...';
-						clear;
-						main
-						break;;
-				esac
-			break;;
-		* ) echo 'invalid response';
-		    clear;
-		    main
-		    break;;
-	esac
 }
 
 main
